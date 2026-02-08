381f2e517ff4366880eacce50762d01eae2356ef
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 17:11:02 +0330

    10

--- GameProject1/cellnode.h
+++ GameProject1/cellnode.h
@@ -5,4 +5,20 @@
 #include <QString>
 #include <QVector>
 
+struct CellNode {
+    QString id;       // A01 ...
+    int shield = 0;   // 0..2
+
+    Player pieceOwner = Player::None;
+    AgentType pieceType = AgentType::None;
+
+    bool markedA = false;
+    bool markedB = false;
+
+    Player controlOwner = Player::None;
+
+    // pointer-based adjacency (مطابق PDF) 8
+    QVector<CellNode*> neighbors;
+};
+
 #endif // CELLNODE_H

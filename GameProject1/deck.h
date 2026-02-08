d61caf3aac838b551ff87f31051ede33601cc687
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 17:29:37 +0330

    23

--- GameProject1/deck.h
+++ GameProject1/deck.h
@@ -6,17 +6,17 @@
 
 class Deck {
 public:
-    void initStandard();      // 4 scout, 3 sniper, 3 sergeant 10
+    void initStandard();
     void shuffle();
 
     AgentType top() const;
-    void rotateTopToBack();   // بعد از اکشن، کارت به انتها 11
+    void rotateTopToBack();
 
-    bool burnOne(AgentType t); // در حمله موفق: یک کارت از نوع هدف حذف شود 12
+    bool burnOne(AgentType t);
     int countOf(AgentType t) const;
 
 private:
     QVector<AgentType> m_cards;
 };
 
-#endif // DECK_H
+#endif

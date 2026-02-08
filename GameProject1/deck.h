8728cc0c20d904ed39d4c5506d2e6aafdedc04d7
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 17:28:42 +0330

    22

--- GameProject1/deck.h
+++ GameProject1/deck.h
@@ -4,5 +4,19 @@
 #include "enums.h"
 #include <QVector>
 
+class Deck {
+public:
+    void initStandard();      // 4 scout, 3 sniper, 3 sergeant 10
+    void shuffle();
+
+    AgentType top() const;
+    void rotateTopToBack();   // بعد از اکشن، کارت به انتها 11
+
+    bool burnOne(AgentType t); // در حمله موفق: یک کارت از نوع هدف حذف شود 12
+    int countOf(AgentType t) const;
+
+private:
+    QVector<AgentType> m_cards;
+};
 
 #endif // DECK_H

ffb069ca1c54004ea97df96ae388f51afabc2222
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 17:32:12 +0330

    25

--- GameProject1/deck.cpp
+++ GameProject1/deck.cpp
@@ -1,2 +1,10 @@
 #include "deck.h"
 #include <QRandomGenerator>
+
+void Deck::initStandard()
+{
+    m_cards.clear();
+    for(int i=0;i<4;i++) m_cards.push_back(AgentType::Scout);
+    for(int i=0;i<3;i++) m_cards.push_back(AgentType::Sniper);
+    for(int i=0;i<3;i++) m_cards.push_back(AgentType::Sergeant);
+}

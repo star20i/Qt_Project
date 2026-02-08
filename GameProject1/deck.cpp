35e12c130f12bdb1aa4762634b738eaae7d14e25
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 17:33:18 +0330

    26

--- GameProject1/deck.cpp
+++ GameProject1/deck.cpp
@@ -8,3 +8,11 @@ void Deck::initStandard()
     for(int i=0;i<3;i++) m_cards.push_back(AgentType::Sniper);
     for(int i=0;i<3;i++) m_cards.push_back(AgentType::Sergeant);
 }
+
+void Deck::shuffle()
+{
+    for(int i=m_cards.size()-1;i>0;--i){
+        int j = QRandomGenerator::global()->bounded(i+1);
+        m_cards.swapItemsAt(i,j);
+    }
+}

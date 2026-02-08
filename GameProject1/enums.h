0072673654bc72d37d8dece8f9228e6648f40af7
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 16:47:09 +0330

    2

--- GameProject1/enums.h
+++ GameProject1/enums.h
@@ -7,7 +7,11 @@
 enum class Player { None=0, A=1, B=2 };
 enum class AgentType { None=0, Scout, Sniper, Sergeant };
 
-
+inline Player playerFromChar(QChar c){
+    if (c=='A') return Player::A;
+    if (c=='B') return Player::B;
+    return Player::None;
+}
 
 
 

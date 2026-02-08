7cd9693c5954039fb17df65b84f0fdaf121fa372
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 18:28:16 +0330

    48

--- GameProject1/gamestate.cpp
+++ GameProject1/gamestate.cpp
@@ -2,3 +2,13 @@
 #include <QSet>
 #include <QHash>
 #include <QRandomGenerator>
+
+void GameState::initNewGame()
+{
+    deckA.initStandard();
+    deckB.initStandard();
+    deckA.shuffle();
+    deckB.shuffle();
+    currentPlayer = Player::A;
+    startTurn();
+}

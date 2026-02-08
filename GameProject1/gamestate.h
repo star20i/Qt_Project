b4eb395f96baf7cd516c31e7618a862665cca2b4
mostafa samei <arefeh82sameei@gmail.com>
2026-02-08 18:22:22 +0330

    44

--- GameProject1/gamestate.h
+++ GameProject1/gamestate.h
@@ -5,5 +5,14 @@
 #include "deck.h"
 #include <QVector>
 
+struct AttackResult {
+    bool ok = false;         // آیا اکشن معتبر بود؟
+    bool success = false;
+    int criteria = 0;
+    QVector<int> rolls;
+    AgentType burnedType = AgentType::None;
+    bool pieceRemoved = false;
+};
+
 
 #endif // GAMESTATE_H

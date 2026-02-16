93b4f943f72d37973ea863187fc7697b7a822d52
star20i <setarehqolami20@gmail.com>
2026-02-16 19:53:14 +0330

    already marked

--- GameProject1/maingamepage.cpp
+++ GameProject1/maingamepage.cpp
@@ -497,3 +497,6 @@ void MainGamePage::onTileClicked(const QString &id)
     if(m_mode==Mode::PickScoutForMark){
         if(!(n->pieceOwner==gs.currentPlayer && n->pieceType==AgentType::Scout)) return;
         if(!gs.scoutMarkCurrent(id)){
+            QMessageBox::information(this, "Scout", "This cell is already marked for you.");
+            return;
+        }
